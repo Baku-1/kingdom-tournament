ed78b5dbeabe0ad704f7eec7cfce741c2dcfa034
Baku-1 <jeremygordon62716@gmail.com>
2025-05-08 21:36:05 -0500

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    NEXT_PUBLIC_NETWORK: process.env.NEXT_PUBLIC_NETWORK,
+    NEXT_PUBLIC_TOURNAMENT_ESCROW_ADDRESS: process.env.NEXT_PUBLIC_TOURNAMENT_ESCROW_ADDRESS,
+    NEXT_PUBLIC_RONIN_RPC_URL: process.env.NEXT_PUBLIC_RONIN_RPC_URL,
+  },
 };
 
 export default nextConfig;
